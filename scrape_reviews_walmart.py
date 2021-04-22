64f0160e26730abb1f775777ebe9e5b3d4fc2381
Thyrsos Makloklas <thirsos@hotmail.com>
2021-04-22 01:44:05 +0100

    Update scrape_reviews_walmart.py

--- scrape_reviews_walmart.py
+++ scrape_reviews_walmart.py
@@ -9,35 +9,64 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+    
+def get_reviews_walmart(page_url):
+    for page in range(1, 4):        
+        page_url = page_url[0:-1] + str(page)
+        response = requests.get(page_url)
+        html = response.text
+        
+        soup = BeautifulSoup(html, "html5lib")
+        
+        review_sections = soup.select(".Grid.ReviewList-content")
+        
+        #Listings document
+        for i, listing in enumerate(review_sections):
+            try:
+                review_text = listing.find(class_="review-text").text
+                review_text = review_text.replace("See more", "").strip()
+            except:
+                pass
+            
+            try: 
+                review_title = listing.find(class_="review-title").text
+            except:
+                pass
+                
+            author = listing.find(class_="review-footer-userNickname").text            
+            star_rating = listing.find(class_="average-rating").text
+            star_rating = float(re.sub('[()]', '', star_rating))
+            source = "walmart"
+                        
+            helpful_upvotes = re.search(r'\((.*?)\)', 
+                                        listing.find(class_='yes-vote').text).group(1)
+            unhelpful_upvotes = re.search(r'\((.*?)\)', 
+                                          listing.find(class_='no-vote').text).group(1)
+            
+            date_created = listing.find(class_='review-date-submissionTime').text
+            datetime_scraped = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+            
+            if 'digital' in soup.find(class_="prod-ProductTitle").text.lower():
+                model = 'Sony PlayStation 5 Digital Edition'
+            else:
+                model = 'Sony PlayStation 5 Disc Edition'
+            
+            review_params = {}    
+            for variable in ["review_title", "review_text", "author", "star_rating",
+                             "verified_review", "helpful_upvotes", "unhelpful_upvotes", 
+                             "model", "date_created", "datetime_scraped", "source"]:
+                review_params[variable] = eval(variable)
+                
+            yield(review_params)
+            
+            time.sleep(1)
 
 #Disk Edition
-url = "https://www.walmart.com/reviews/product/363472942"
-response = requests.get(url)
-html = response.text
-
-soup = BeautifulSoup(html, "html5lib")
-
-review_sections = soup.select(".Grid.ReviewList-content")
-
-for i, listing in enumerate(review_sections):
-    review_text = listing.find(class_="review-text").text
-    review_text = review_text.replace("See more", "").strip()
-        
-    seller_name = listing.find(class_="review-footer-userNickname").text
-    review_title = listing.find(class_="review-title").text
-    star_rating = listing.find(class_="average-rating").text
-    star_rating = float(re.sub('[()]', '', star_rating))
+url = "https://www.walmart.com/reviews/product/363472942?page=1"
+for i in get_reviews_walmart(url):
+    print(i)
     
-    helpful_upvotes = re.search(r'\((.*?)\)', 
-                                listing.find(class_='yes-vote').text).group(1)
-    unhelpful_upvotes = re.search(r'\((.*?)\)', 
-                                  listing.find(class_='no-vote').text).group(1)
-    date = listing.find(class_='review-date-submissionTime').text
-
-    a_dict = {}    
-    for variable in ["review_text", "author", "star_rating", "condition",
-                     "attributes", "verified_review", "seller_name", 
-                     "helpful_unhelpful", "date"]:
-        a_dict[variable] = eval(variable)
-    
-    print(a_dict)
+#Digital Edition
+url = "https://www.walmart.com/reviews/product/493824815?page=1"
+for i in get_reviews_walmart(url):
+    print(i)
